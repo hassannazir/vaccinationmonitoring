090a33311415cf79c37774999c6d769dcb970f95
hassan.nazir1998 <hassan.nazir1998@gmail.com>
2020-07-18 10:30:16 +0500

    Add constants for user roles

--- vaccinationmonitoring/Helpers/AppConstants.cs
+++ vaccinationmonitoring/Helpers/AppConstants.cs
@@ -10,5 +10,17 @@ namespace vaccinationmonitoring.Helpers
         public const string BaseUrl = "https://localhost:44333/";
         public const string ok = "Ok";
         public const string notOk = "Error";
+        public const string SuccessMessage = "success";
+        public const string ErrorMessage = "error";
+        public const int RoleParent = 0;
+        public const int RoleWorker = 1;
+        public const int RoleAdmin = 2;
+        public const string Parent = "Parent";
+        public const string Worker = "Worker";
+        public const string Admin = "Admin";
+        public const int UserNotApprovedStatus = 0;
+        public const string UNotApprovedStatus = "Not Approved";
+        public const int UserApprovedStatus = 1;
+        public const string UApprovedStatus = "Approved";
     }
 }
